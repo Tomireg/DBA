ef6f77e94176a4b100610f6071028e966b54142d
Tomireg <146027113+Tomireg@users.noreply.github.com>
2024-06-04 16:40:04 +0100

    testing app

--- flask/app.py
+++ flask/app.py
@@ -1,16 +1,112 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import text
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://testuser:test123@localhost/dvdrental"
 
-db = SQLAlchemy()
-db.init_app(app)
-
-@app.route("/")
-def index():
-    user_data = db.session.execute(text("SELECT * FROM user_names"))
-    return render_template("index.html", users=user_data)
-
-if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+from flask import Flask, render_template, request, redirect, url_for 
+import psycopg2 
+  
+app = Flask(__name__) 
+  
+conn = psycopg2.connect(database="flask_db", user="postgres", 
+                        password="root", host="localhost", port="5432") 
+  
+cur = conn.cursor() 
+   
+cur.execute( 
+    '''CREATE TABLE IF NOT EXISTS products (id serial \ 
+    PRIMARY KEY, name varchar(100), price float);''') 
+  
+cur.execute( 
+    '''INSERT INTO products (name, price) VALUES \ 
+    ('Apple', 1.99), ('Orange', 0.99), ('Banana', 0.59);''') 
+  
+conn.commit() 
+   
+cur.close() 
+conn.close() 
+  
+  
+@app.route('/') 
+def index():  
+    conn = psycopg2.connect(database="flask_db", 
+                            user="postgres", 
+                            password="root", 
+                            host="localhost", port="5432") 
+   
+    cur = conn.cursor() 
+   
+    cur.execute('''SELECT * FROM products''') 
+  
+    data = cur.fetchall() 
+   
+    cur.close() 
+    conn.close() 
+  
+    return render_template('index.html', data=data) 
+  
+  
+@app.route('/create', methods=['POST']) 
+def create(): 
+    conn = psycopg2.connect(database="flask_db", 
+                            user="postgres", 
+                            password="root", 
+                            host="localhost", port="5432") 
+  
+    cur = conn.cursor() 
+   
+    name = request.form['name'] 
+    price = request.form['price'] 
+  
+    cur.execute( 
+        '''INSERT INTO products \ 
+        (name, price) VALUES (%s, %s)''', 
+        (name, price)) 
+  
+    conn.commit() 
+   
+    cur.close() 
+    conn.close() 
+  
+    return redirect(url_for('index')) 
+  
+  
+@app.route('/update', methods=['POST']) 
+def update(): 
+    conn = psycopg2.connect(database="flask_db", 
+                            user="postgres", 
+                            password="root", 
+                            host="localhost", port="5432") 
+  
+    cur = conn.cursor() 
+   
+    name = request.form['name'] 
+    price = request.form['price'] 
+    id = request.form['id'] 
+   
+    cur.execute( 
+        '''UPDATE products SET name=%s,\ 
+        price=%s WHERE id=%s''', (name, price, id)) 
+   
+    conn.commit() 
+    return redirect(url_for('index')) 
+  
+  
+@app.route('/delete', methods=['POST']) 
+def delete(): 
+    conn = psycopg2.connect 
+    (database="flask_db", user="postgres", 
+     password="root", 
+     host="localhost", port="5432") 
+    cur = conn.cursor() 
+  
+    id = request.form['id'] 
+  
+    cur.execute('''DELETE FROM products WHERE id=%s''', (id,)) 
+  
+    conn.commit() 
+   
+    cur.close() 
+    conn.close() 
+  
+    return redirect(url_for('index')) 
+  
+  
+if __name__ == '__main__': 
+    app.run(debug=True) 
