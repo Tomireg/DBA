9effe389e0efc4732d59db55cfe6bfc43237e940
Tomireg <146027113+Tomireg@users.noreply.github.com>
2024-06-04 16:46:50 +0100

    sdf

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,16 @@
 
+conn = psycopg2.connect(database="flask_db",  
+                        user="postgres", 
+                        password="root",  
+                        host="localhost", port="5432") 
+  
+cur = conn.cursor() 
+  
+conn.commit() 
+  
+cur.close() 
+conn.close() 
+
 from flask import Flask, render_template, request, redirect, url_for 
 import psycopg2 
   
