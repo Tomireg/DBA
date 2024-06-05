a34757bdf961ab777ebf96c7bc9c827294331c43
Tomireg <146027113+Tomireg@users.noreply.github.com>
2024-06-05 11:51:18 +0100

    dark mode test

--- flask/static/js/script.js
+++ flask/static/js/script.js
@@ -1,1 +1,45 @@
+document.addEventListener("DOMContentLoaded", init);
+function init() {
+    let darkModeStore = localStorage.getItem('dark-mode');
 
+    if (darkModeStore === null) {
+        localStorage.setItem("dark-mode", false);
+    }
+    if (darkModeStore === "true") {
+        setDarkMode();
+    } else {
+        ;
+    }
+
+    let myButton = document.getElementById("mybtn");
+    myButton.addEventListener("click", clickedDarkMode);
+}
+
+function clickedDarkMode() {
+    toggleDarkMode();
+    setDarkMode();
+}
+
+function toggleDarkMode() {
+    if (localStorage.getItem("dark-mode") === "true") {
+        localStorage.setItem("dark-mode", false);
+
+    } else {
+        localStorage.setItem("dark-mode", true);
+    }
+}
+
+function setDarkMode() {
+    styleBody();
+    styleBtn();
+}
+
+function styleBody() {
+    let element = document.body;
+    element.classList.toggle("dark-mode");
+}
+
+function styleBtn() {
+    let dmBtn = document.getElementById("mybtn");
+    dmBtn.classList.toggle("dmbtn");
+}
